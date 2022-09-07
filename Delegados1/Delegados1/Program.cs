c18df0dc8ddaf0b39fa9cd641670a20fe580be0b
Jonathan <blackfury0399@gmail.com>
2022-09-06 23:53:17 -0600

    Delegados1

---
+++ Delegados1/Delegados1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Delegados1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creación del objeto delegado apuntando a MensajeBienvenida
+            ObjetoDelegado ElDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
+
+            //Utilización del delegado para llamar al método SaludoBienvenida
+            ElDelegado();
+
+
+            /************///Ahora apuntamos al otro método de la otra clase
+            ElDelegado = new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            ElDelegado();
+        }
+
+        //definición del objeto delegado según la estructura de los métodos a llamar
+        delegate void ObjetoDelegado();
+    }
+
+    class MensajeBienvenida
+    {
+        public static void SaludoBienvenida() 
+        {
+            Console.WriteLine("Método saludar");
+        }    
+    }
+
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida()
+        {
+            Console.WriteLine("Método de despedida");
+        }
+    }
+}
