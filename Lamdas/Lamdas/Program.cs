0ea151fa4912cb65d03aaafcfcf87bac5a489a42
Jonathan <blackfury0399@gmail.com>
2022-09-08 00:34:44 -0600

    Ejercicios Lamdas

---
+++ Lamdas/Lamdas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lamdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso del delegado Lamda, presindimos de las funciones
+
+            OperacionesMatematicas operacion = new OperacionesMatematicas(/*Cuadrado*/ num => num*num);//Con la expresión Lambda se reducirá el código
+            Console.WriteLine(operacion(4));
+
+
+            OperacionMatematica operacion2 = new OperacionMatematica(/*Suma*/ (num1, num2) => num1 + num2); //cuando son dos parametros estos van entre parentesis
+            Console.WriteLine(operacion2(5,3));
+        }
+
+
+
+        //public static int Cuadrado(int num) 
+        //{
+        //    return num * num;
+        //}
+        public delegate int OperacionesMatematicas(int numero);
+                                                                     //CON DOS PARAMETROS /****/
+        //public static int Suma(int num1, int num2) 
+        //{
+        //    return num1 + num2;
+        //}
+        public delegate int OperacionMatematica(int numero1, int numero2);
+    }
+}
---
+++ Lamdas2/Lamdas2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lamdas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            p1.Nombre = "Jonathan";
+            p1.Edad = 23;
+
+            Persona p2 = new Persona();
+            p2.Nombre = "Daniela";
+            p2.Edad = 35;
+
+            /*Usar Delegado con la expresión Lamda*/
+            ComparaPersonas comparaEdad = (persona1, persona2) => persona1 == persona2;
+
+            Console.WriteLine(comparaEdad(p1.Edad, p2.Edad));
+
+
+
+            /********************Conocer si hay numeros pares en la lista************************/
+
+            //List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //List<int> numerosPares = numeros.FindAll(i => i % 2 == 0);
+
+            //foreach (int pares in numerosPares)
+            //{
+            //    Console.WriteLine(pares);
+            //}
+        }
+
+        public delegate bool ComparaPersonas(int edad1, int edad2);
+    }
+
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
