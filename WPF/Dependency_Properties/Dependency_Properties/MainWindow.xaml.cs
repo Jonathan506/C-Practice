6220c92d78c5b13d8974cff60c68c96bca732c0c
Jonathan <blackfury0399@gmail.com>
2022-09-12 22:52:10 -0600

    wps examples

---
+++ WPF/Dependency_Properties/Dependency_Properties/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dependency_Properties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /***********************Creando una Dependency Propertie*****************************/
+        public int MiProperty
+        {
+            get { return (int)GetValue(MiDependencyProperty); }
+            set { SetValue(MiDependencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty MiDependencyProperty = DependencyProperty.Register("MiProperty",typeof(int),typeof(MainWindow), new PropertyMetadata(0) );
+    }
+}
---
+++ WPF/I_Notify_Property_Changef/I_Notify_Property_Changef/clsNombre.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Notify_Property_Changef
+{
+    public  class clsNombre : INotifyPropertyChanged
+    {
+        private string nombre;
+        private string apellido;
+        private string nombreCompleto;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property) 
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Nombre 
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged("NombreCompleto");   }
+        }
+        public string Apellido 
+        {
+            get { return apellido; }
+            set { apellido = value;
+                OnPropertyChanged("NombreCompleto");
+            }
+        }
+        public string NombreCompleto 
+        {
+            get { nombreCompleto=Nombre + " " + Apellido;
+                return nombreCompleto;
+            }
+            set { nombreCompleto = value;
+                nombre = nombreCompleto.Split(' ')[0];
+                apellido = nombreCompleto.Split(' ')[1];
+                OnPropertyChanged("Nombre");
+                OnPropertyChanged("Apellido");
+            }
+        }
+    }
+}
