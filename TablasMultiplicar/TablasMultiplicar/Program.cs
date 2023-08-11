d3a11010966960769fcce5eb8990e4976f5eb616
Jonathan <blackfury0399@gmail.com>
2023-08-10 23:46:29 -0600

    Practica TablasMultiplicar

--- TablasMultiplicar/TablasMultiplicar/Program.cs
+++ TablasMultiplicar/TablasMultiplicar/Program.cs
@@ -6,23 +6,47 @@ namespace TablasMultiplicar
     {
         static void Main(string[] args)
         {
+            inicio:
+            int num  = 1;
+
             Console.WriteLine("Este programa muestra las tablas de multiplicar según el número indicado");
             Console.WriteLine();
-            Console.WriteLine("Digite un número del 1 al 12 para visualizar la respectiva tabla de valores");
-
-            int num = Int32.Parse(Console.ReadLine());
-
-            if (num > 0 && num < 13)
+            try
             {
-                for (int i = 1; i <= 12; i++)
+                Console.WriteLine("Digite un número del 1 al 12 para visualizar la respectiva tabla de valores");
+
+                num = Int32.Parse(Console.ReadLine());
+
+                TablaMulti(num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("El valor debe ser un dato númerico");
+            }
+            finally
+            {
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            goto inicio;
+
+            void TablaMulti(int Num) 
+            {
+                if (Num > 0 && Num < 13)
                 {
-                    int tabla = i * num;
-                    Console.WriteLine(num + " * " + i + " = " + tabla );
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        int tabla = i * Num;
+                        Console.WriteLine(Num + " * " + i + " = " + tabla);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El valor proporcionado está fuera del rango de valores establecido");
                 }
             }
-            else {
-                Console.WriteLine("El valor proporcionado está fuera del rango de valores establecido");
-            }
+
         }
     }
 }
